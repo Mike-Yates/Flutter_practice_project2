31f6214e44f15e18a4aedbdcdf86455d9ba7fcf1
Mike-Yates <70710481+Mike-Yates@users.noreply.github.com>
2022-01-04 22:48:54 -0500

    Restart with fresh app
    defined multiple screens for the 3 pages of our app
    Plan is to make an app that lets a user change locations and see what time it is where he is.

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,8 @@
 import 'package:flutter/material.dart';
-import 'quote.dart';
-import 'quote_card.dart';
+import 'screens/home.dart';
 
 void main() {
   runApp(const MaterialApp(
-    home: WidgetTree(),
+    home: Home(),
   ));
-}
-
-class WidgetTree extends StatefulWidget {
-  const WidgetTree({Key? key}) : super(key: key);
-
-  @override
-  _WidgetTreeState createState() => _WidgetTreeState();
-}
-
-class _WidgetTreeState extends State<WidgetTree> {
-  List<Quote> quotes =
-  [
-    Quote(text: 'super inspiring quote 1', author: 'Mike'),
-    Quote(text: 'Amazingly worded num 2', author: "Mik"),
-    Quote(text: 'another quote 3', author: 'Mikee'),
-  ];
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        title: Text('Quotes'),
-        //backgroundColor: Colors.grey[400],
-        centerTitle: true,
-        backgroundColor: Colors.amberAccent,
-      ),
-      body:Column(
-        children: quotes.map((quote) => QuoteCard(
-            quote: quote,
-            delete: (){  // pass a function as a parameter
-              setState(() {
-                quotes.remove(quote);
-              });
-          },
-        )).toList(),
-      )
-    );
-  }
 }
---
+++ lib/screens/choose_location.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class locationSelection extends StatefulWidget {
+  const locationSelection({Key? key}) : super(key: key);
+
+  @override
+  _locationSelectionState createState() => _locationSelectionState();
+}
+
+class _locationSelectionState extends State<locationSelection> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
