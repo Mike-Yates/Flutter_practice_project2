43f16d734393a7ca5539ff5b6cdc7a56c67e8233
Mike-Yates <70710481+Mike-Yates@users.noreply.github.com>
2022-01-05 12:28:49 -0500

    part 24
    Added a button incrementer and uses setState(). learned about overriding a stateful widget's initState function.

--- lib/screens/choose_location.dart
+++ lib/screens/choose_location.dart
@@ -8,8 +8,17 @@ class LocationSelection extends StatefulWidget {
 }
 
 class _LocationSelectionState extends State<LocationSelection> {
+  int counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    print('init state run (debugging)');
+  }
+
   @override
   Widget build(BuildContext context) {
+    print('build ran (debugging)');
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
@@ -18,7 +27,14 @@ class _LocationSelectionState extends State<LocationSelection> {
         elevation: 0.0, // removes shadow, makes it appear flat on page
         title: Text('please choose a location'),
       ),
-      body: Text('we are on the choose location screen. replace this later ')
+      body: TextButton(
+        onPressed: (){
+          setState(() {
+            counter++;
+          });
+        },
+        child: Text('counter: $counter'),
+      )
     );
   }
 }
