0b137984b47be31f49a10b13a57fb6e1fa805fcd
Mike-Yates <70710481+Mike-Yates@users.noreply.github.com>
2022-01-05 14:49:47 -0500

    part 27
    Learned to use the worldtimeapi endpoint to get the time for specific locations. good practice with moving the json string response into a dart Map and then calling on elements by the key

--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -10,16 +10,27 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  void getData() async{
-    Response response = await get(Uri.parse('https://jsonplaceholder.typicode.com/todos/1'));
+  void getTime() async{
+    //Response response = await get(Uri.parse('http://worldtimeapi.org/api/timezone/Europe/London'));
+    // i tested it with America/New_York
+    Response response = await get(Uri.parse('http://worldtimeapi.org/api/timezone/America/New_York'));
     Map data = jsonDecode(response.body);
-    print('the title is: ${data['title']}');
+    print(data);
+
+    DateTime now = DateTime.parse(data['utc_datetime']); // casting in dart is done using parse
+    String offset_str = data['utc_offset'].substring(1,3);
+    int offset_int = int.parse(offset_str);
+    if(data['utc_offset'].substring(0,1) == '-'){
+      offset_int = offset_int * -1;
+    }
+    now = now.add(Duration(hours: offset_int));
+    print('in New York, it is $now');
   }
 
   @override
   void initState() {
     super.initState();
-    getData();
+    getTime();
     print('init state run (debugging)');
   }
 
