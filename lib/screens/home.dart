366e6a0d87d3b8e168d78f933a449748528538ec
Mike-Yates <70710481+Mike-Yates@users.noreply.github.com>
2022-01-05 19:42:53 -0500

    part 31
    formatted dates using intl package. began using textstyles to make the UI better

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -16,18 +17,43 @@ class _HomeState extends State<Home> {
     print(data);
     return Scaffold(
       body: SafeArea(
-          child: Column(
-            children: <Widget>[
-              Text('home screen'),
-              TextButton.icon(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/location');
-                },
-                icon: Icon(Icons.edit_location, color: Colors.green),
-                label: Text('go to location screen', style: TextStyle(color: Colors.green)),
-
-              )
-            ],
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                TextButton.icon(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/location');
+                  },
+                  icon: Icon(Icons.edit_location, color: Colors.grey[900]),
+                  label: Text(
+                      'Edit location',
+                      style: TextStyle(
+                        fontSize: 24.0,
+                        color: Colors.grey[900],
+                      ),
+                  ),
+                ),
+                const SizedBox(height: 20.0),
+                Text(
+                  data['location'],
+                  style: const TextStyle(
+                    fontSize: 28.0,
+                    letterSpacing: 2.0,
+                  )
+                ),
+                const SizedBox(height: 20.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(data['time'], style: const TextStyle(
+                      fontSize: 65.0,
+                    ))
+                  ],
+                ),
+              ],
+            ),
           )
       )
     );
--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -1,5 +1,6 @@
 import 'package:http/http.dart';
 import 'dart:convert';
+import 'package:intl/intl.dart';
 
 class WorldTime{
   String location;  // going to be printed on the screen
@@ -18,13 +19,7 @@ class WorldTime{
       Map data = jsonDecode(response.body);
 
       DateTime now = DateTime.parse(data['utc_datetime']); // casting in dart is done using parse
-      String offset_str = data['utc_offset'].substring(1,3);
-      int offset_int = int.parse(offset_str);
-      if(data['utc_offset'].substring(0,1) == '-'){
-        offset_int = offset_int * -1;
-      }
-      now = now.add(Duration(hours: offset_int));
-      time = now.toString().substring(11,19);  // only get the time
+      time = DateFormat.jm().format(now);
     } catch (e){
       time = 'could not load time properly';  // added 10 characters before hand because
       print('caught error in world_time.dart. error: $e');
